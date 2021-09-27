81bec7bfec8585e98b8ac638a9400eb9286b058f
Sayuck <nobrega.m.roberto@gmail.com>
2021-09-27 10:07:53 -0300

    criando as taks e iniciando o hook do mergesort
    Co-authored-by: EduardoPicolo <eduardonpicolo@gmail.com>

--- DC_Sort_Your_Tasks/src/App.js
+++ DC_Sort_Your_Tasks/src/App.js
@@ -1,9 +1,50 @@
 
+import Button from './components/Button';
+import Input from './components/Input';
+import List from './components/TasksList';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import * as S from './styles/styles';
+import useMergesort from './hooks/useMergesort';
+
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const selecionadas = useMergesort(tasks);
+
+  function onSubmit(data) {
+    setTasks([...tasks, data]);
+  }
+
   return (
-    <div className="App">
-   <h1>Inicio</h1>
-    </div>
+    <S.MainContainer>
+      <S.Content>
+        <S.Form onSubmit={handleSubmit(onSubmit)}>
+          <Input
+            register={register('task', {
+              validate: (value) => !tasks.some((item) => item.task === value) || 'Tarefa já existe!',
+              required: 'Campo obrigatório!',
+            })}
+            placeholder="Task"
+            label="Task"
+            type="text"
+            error={errors.task}
+            autoComplete="off"
+          />
+
+          <Button>Adicionar</Button>
+        </S.Form>
+
+        <List tasks={tasks} deleteTaskCallback={setTasks} />
+        <List tasks={selecionadas} deleteTaskCallback={setTasks}/>
+      </S.Content>
+    </S.MainContainer>
   );
 }
 
